f00c196044ca8ccb9d9da86f55385c8640fbfb9c
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-03-27 21:29:02 -0400

    implementing reactrouter

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import Header from './components/Header'
+import FriendsList from './components/FriendsList'
+import StealPizza from './components/StealPizza'
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <div className="container" />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <div className="container" />
+          <Switch>
+            <Route exact path={['/friends, /']} component={FriendsList} />
+            <Route exact path={['/stealpizza']} component={StealPizza} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 }
