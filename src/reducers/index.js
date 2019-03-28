28520a97095e10d56172d18820f18effcd2b2a5b
marielklem <mariel.steinbeiser@gmail.com>
2019-03-28 10:27:08 -0400

    Updated with friends API to recieve full contact list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,9 @@
 //combine reducers
+import { combineReducers } from "redux";
+import FriendsReducer from './reducer-friends';
+
+const rootReducer = combineReducers({
+  friends: FriendsReducer
+})
+
+export default rootReducer
