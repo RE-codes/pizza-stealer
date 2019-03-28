79e6660905883fa658010afddec4a6ad488058ab
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-03-28 18:52:39 -0400

    creating mock PizzaDetail to practice onclick event

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchFriends } from '../actions';
+import PizzaDetail from './PizzaDetail';
 
 class FriendsList extends Component {
   componentDidMount() {
@@ -14,18 +15,36 @@ class FriendsList extends Component {
     return _.map(this.props.friends, friends => {
       return (
         <li className="list-group-item">
-        <div className="row w-100">
+          <div className="row w-100">
             <div className="col-12 col-sm-6 col-md-3 px-0">
-                <img src={friends.imgUrl} style={{objectFit: "cover"}}alt="Friend's Picture" class="rounded-circle mx-auto d-block img-fluid" width="100px" height="100px"/>
+              <img
+                src={friends.imgUrl}
+                style={{ objectFit: 'cover' }}
+                alt=""
+                className="rounded-circle mx-auto d-block img-fluid"
+                width="100px"
+                height="100px"
+              />
             </div>
             <div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
-                <label className="name lead">{friends.name}</label>
-                <br/> 
-                <span className="fa fa-map-marker-alt fa-fw text-muted" title=""></span>
-                <span className="text-muted">{friends.address}</span>
-                <br/>
-                <span className="fa fa-phone fa-fw text-muted" title=""></span>
-                <span className="text-muted small">{friends.phone}</span>
+              <label className="name lead">{friends.name}</label>
+              <br />
+              <span
+                className="fa fa-map-marker-alt fa-fw text-muted"
+                title=""
+              />
+              <span className="text-muted">{friends.address}</span>
+              <br />
+              <span className="fa fa-phone fa-fw text-muted" title="" />
+              <span className="text-muted small">{friends.phone}</span>
+              <button onClick={onClickButton} className="rounded">
+                <img
+                  src="https://i0.wp.com/www.pizzajerkpdx.com/wp-content/themes/pizza-jerk-theme/assets/images/slice.png?w=600"
+                  width="30"
+                  height="30"
+                  alt=""
+                />
+              </button>
             </div>
           </div>
         </li>
@@ -37,11 +56,21 @@ class FriendsList extends Component {
     return (
       <section className="full-page padding-lg">
         <div className="container"> {this.renderFriends()} </div>
+        {/* <PizzaDetail /> */}
       </section>
     );
   }
 }
 
+const onClickButton = e => {
+  e.preventDefault();
+  console.log('hey!');
+  return (
+    <div>
+      <PizzaDetail />
+    </div>
+  );
+};
 function mapStateToProps(state) {
   return {
     friends: state.friends
