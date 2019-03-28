6964d953cb8e6f69864c7d938addff7f60ea12ef
Richard Eldridge <eldridge.rich@gmail.com>
2019-03-28 15:28:47 -0400

    mounted pizzaFriendsList to the DOMMMMM

--- src/components/PizzaFriendsList.js
+++ src/components/PizzaFriendsList.js
@@ -1,39 +1,34 @@
-import React from 'react';
-import PizzaFriend from './PizzaFriend';
-import fetchPizza from '../actions/index';
+import React, { Component } from 'react';
+// import PizzaFriend from './PizzaFriend';
+import { fetchPizza } from '../actions/index';
 import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-
-const PizzaFriendsList = props => {
-  createPizzaFriendsList = () => {
+class PizzaFriendsList extends Component {
+  createPizzaFriendsList = props => {
     // This function should take the contactsList and run all the phone numbers through the fetch request to determine who has pizza on the way.
-
-
-  }
+  };
   // It should return an array of objects containing the OrderStatus.phone, the OrderStatus.OrderDescription, and the OrderStatus.OrderStatus
-
-
-
-
   render() {
     return (
       <div>
-
         <h1>A list of pizza bearing friends goes in here</h1>
       </div>
-    )
+    );
   }
+}
 
-};
-
-mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     pizzaFriends: state.pizzaFriends
-  }
-}
+  };
+};
 
-mapDispatchtoProps = (dispatch) => {
-  return bindActionCreators({ fetchPizza }, dispatch)
-}
+const mapDispatchtoProps = dispatch => {
+  return bindActionCreators({ fetchPizza }, dispatch);
+};
 
-export default connect(mapStateToProps, mapDispatchtoProps)(PizzaFriendsList)
+export default connect(
+  mapStateToProps,
+  mapDispatchtoProps
+)(PizzaFriendsList);
--- src/components/StealPizza.js
+++ src/components/StealPizza.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import PizzaDetail from './PizzaDetail';
+import PizzaFriendsList from './PizzaFriendsList';
 
 export class StealPizza extends Component {
   render() {
     return (
       <div style={{ marginTop: '75px' }}>
+        <PizzaFriendsList />
         <PizzaDetail />
       </div>
     );
