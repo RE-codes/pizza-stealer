32f14efc5e12b6f27d9ccb95cfce971bcec35666
Richard Eldridge <eldridge.rich@gmail.com>
2019-03-29 10:10:09 -0400

    removed lodash from reducer-friends and FriendsList

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -10,13 +9,13 @@ class FriendsList extends Component {
   }
 
   renderFriends() {
-    return _.map(this.props.friends, friends => {
+    return this.props.friends.map(friend => {
       return (
-        <li className="list-group-item" key={friends.id}>
+        <li className="list-group-item" key={friend.id}>
           <div className="row w-100">
             <div className="col-12 col-sm-6 col-md-3 px-0">
               <img
-                src={friends.imgUrl}
+                src={friend.imgUrl}
                 style={{
                   objectFit: 'cover'
                 }}
@@ -25,15 +24,15 @@ class FriendsList extends Component {
               />
             </div>
             <div className="col-12 col-sm-6 col-md-9 text-center text-sm-left">
-              <label className="name lead"> {friends.name} </label> <br />
+              <label className="name lead"> {friend.name} </label> <br />
               <span className="fa fa-map-marker-alt fa-fw text-muted" title="">
                 {' '}
               </span>{' '}
-              <span className="text-muted"> {friends.address} </span> <br />
+              <span className="text-muted"> {friend.address} </span> <br />
               <span className="fa fa-phone fa-fw text-muted" title="">
                 {' '}
               </span>{' '}
-              <span className="text-muted small"> {friends.phone} </span>{' '}
+              <span className="text-muted small"> {friend.phone} </span>{' '}
             </div>{' '}
           </div>{' '}
         </li>
