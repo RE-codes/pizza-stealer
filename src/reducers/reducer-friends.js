78930fc2f0130249772c81b4e6c8c14a883e5836
marielklem <mariel.steinbeiser@gmail.com>
2019-03-29 09:56:24 -0400

    fixed filter error and header error

--- src/reducers/reducer-friends.js
+++ src/reducers/reducer-friends.js
@@ -4,7 +4,7 @@ import { FETCH_FRIENDS } from '../actions';
 export default function(state = [], action) {
   switch (action.type) {
     case FETCH_FRIENDS:
-      return _.mapKeys(action.payload.data.contactList, "id");
+      return action.payload.data.contactList;
     default:
       return state;
   }
