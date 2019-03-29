a0e0b906b12ff0e1a022dc87399f5f5480330d86
Richard Eldridge <eldridge.rich@gmail.com>
2019-03-28 20:38:31 -0400

    finally got create pizza friends working

--- src/components/PizzaFriendsList.js
+++ src/components/PizzaFriendsList.js
@@ -1,32 +1,64 @@
 import React, { Component } from 'react';
-import { fetchPizza } from '../actions/index';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
 class PizzaFriendsList extends Component {
-  // componentDidMount() {
-  //   this.props.fetchPizza(9192717815);
-  // }
-
-  createPizzaFriendsList = () => {
-    const pizzaFriends = this.props.friends.filter(async friend => {
-      const resp = await fetch(this.props.fetchPizza(9192717815));
-      const json = await resp.json();
-
-      console.log(pizzaFriends);
-      if (json.data.contactList.phone === friend.phone) return friend;
-    });
+  state = {
+    pizzaFriends: []
   };
 
+  componentDidMount() {
+    this.getPizzaFriends();
+  }
   // This function should take the contactsList and run all the phone numbers through the fetch request to determine who has pizza on the way. The OrderStatus.OrderStatus needs to be NOT undefined and NOT 'complete'.
 
   // It should return an array of objects containing the OrderStatus.phone, the OrderStatus.OrderDescription, and the OrderStatus.OrderStatus
+  getPizzaFriends = () => {
+    this.props.friends.filter(async friend => {
+      // make api call
+      const resp = await fetch(
+        `https://my-json-server.typicode.com/marielklem/pizza-api/db`
+      );
+      const data = await resp.json();
+
+      if (
+        data.Envelope.Body.GetTrackerDataResponse.OrderStatuses.OrderStatus
+          .Phone === friend.phone &&
+        data.Envelope.Body.GetTrackerDataResponse.OrderStatuses.OrderStatus
+          .OrderStatus !== 'Complete'
+      ) {
+        friend.pizzaType =
+          data.Envelope.Body.GetTrackerDataResponse.OrderStatuses.OrderStatus.OrderDescription;
+
+        friend.deliveryDriver =
+          data.Envelope.Body.GetTrackerDataResponse.OrderStatuses.OrderStatus
+            .DriverName || null;
+
+        friend.orderStatus =
+          data.Envelope.Body.GetTrackerDataResponse.OrderStatuses.OrderStatus.OrderStatus;
+
+        friend.timeOrdered =
+          data.Envelope.Body.GetTrackerDataResponse.OrderStatuses.OrderStatus.StartTime;
+
+        this.setState({ pizzaFriends: [...this.state.pizzaFriends, friend] });
+      }
+    });
+  };
+
+  renderPizzaFriends = () => {
+    return this.state.pizzaFriends.map(pizzaFriend => {
+      return (
+        <div>
+          <h1>This is where the pizza friends live.</h1>
+          <h4>{pizzaFriend.name}</h4>
+          <p>{pizzaFriend.address}</p>
+          <p>{pizzaFriend.phone}</p>
+        </div>
+      );
+    });
+  };
+
   render() {
-    return (
-      <div>
-        <h1>A list of pizza bearing friends goes in here</h1>
-      </div>
-    );
+    return <div>{this.renderPizzaFriends()}</div>;
   }
 }
 
@@ -37,11 +69,8 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchtoProps = dispatch => {
-  return bindActionCreators({ fetchPizza }, dispatch);
-};
+// const mapDispatchtoProps = dispatch => {
+//   return bindActionCreators({ fetchPizza }, dispatch);
+// };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchtoProps
-)(PizzaFriendsList);
+export default connect(mapStateToProps)(PizzaFriendsList);
